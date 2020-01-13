bb43d9f64b41dc3d9e33279af60c68608b4fdc00
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-01-13 14:38:55 +0900

    스도쿠 문제 수정
    만약 아니면 return 백트래킹 , 맞으면 출력

--- Sudoku.java
+++ Sudoku.java
@@ -7,11 +7,11 @@ import java.io.OutputStreamWriter;
 public class Sudoku {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static int[][] moveArray = {{1,0},{1,1},{0,1},{-1,0},{-1,-1},{0,-1},{1,-1},{-1,1}};
-	static int[][] board;
-	static boolean[][] isFinish;
-	static boolean row[][] = new boolean[9][9]; //가로
-	static boolean column[][] = new boolean[9][9]; //세로
-	static boolean square[][] = new boolean[9][9]; //사각형 3X3 
+	static int[][] board = new int[9][9];
+	static boolean row[][] = new boolean[10][10]; //가로
+	static boolean column[][] = new boolean[10][10]; //세로
+	static boolean square[][] = new boolean[9][10]; //사각형 3X3 
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,37 +20,86 @@ public class Sudoku {
 			String[] line = buffer.split(" ");
 			for(int j=0; j<9; j++) {
 				int data = Integer.parseInt(line[j]);
-				if(data==0) {
-					isFinish[i][j] = false;
-				} else {
-					isFinish[i][j] = true;
-				}
 				board[i][j] = data;
+				if(data!=0) {
+					row[i][data] = true;
+					column[j][data] = true;
+					square[square(i,j)][data] = true;
+				}
 			}	
-		}			
+		}
+
+		Sudoku(0,0);
 	}
 	
-	public static int square(int i, int j) {
-		return i+j; 
+	private static int square(int i, int j) {
+		return j/3 + (i/3) * 3; 
 	}
 	
-	public static void Sudoku(int i, int j) {
+	private static void printSudoku() {
+		for(int i=0; i<9; i++) {
+			for(int j=0; j<9; j++) {
+				if(j==8) {
+					System.out.print(board[i][j]);
+				} else {
+					System.out.print(board[i][j]+ " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	private static boolean checkSudoku() {
+		boolean isFinish = true;
+		for(int i=0; i<9; i++) {
+			for(int j=1; j<10; j++) {
+				if(row[i][j] != true) {
+					isFinish = false;
+					break;
+				}
+			}
+			if(isFinish) break;
+		}
+		
+		return isFinish;
+	}
+	
+	private static void Sudoku(int i, int j) {
 		
 		if(i == 9) {
-			//스도쿠 출력 
-			System.exit(0);// 프로그램 종료
+			//if(checkSudoku()) {
+				printSudoku();
+				System.exit(0);// 프로그램 종료
+			//} else {
+			//	return;
+			//}
 		}
 		
 		if(board[i][j] > 0) {
+			if(j==8) {
+				Sudoku(i+1,0);
+			}
+			else {
+				Sudoku(i,j+1);
+			}
 			
 		} else {
 			for(int k=1; k<10; k++) {
-				if(j==8) {
-					Sudoku(i+1,0);
-				}
-				else {
-					Sudoku(j,i+1);
-				}
+				if(!row[i][k] && !column[j][k] && !square[square(i,j)][k])
+					if(j==8) {
+						board[i][j] = k;
+						row[i][k] = column[j][k] = square[square(i,j)][k] = true;
+						Sudoku(i+1,0);
+						//board[i][j] = 0;
+						//row[i][k] = column[j][k] = square[square(i,j)][k] = false;
+					}
+					else {
+						board[i][j] = k;
+						row[i][k] = column[j][k] = square[square(i,j)][k] = true;
+						Sudoku(i,j+1);
+						//board[i][j] = 0;
+						//row[i][k] = column[j][k] = square[square(i,j)][k] = false;
+					}
 			}
 		}
 		
