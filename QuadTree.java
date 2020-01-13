6cd26cc622134f2fc3bc0447b45f833667adce50
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-01-14 00:33:20 +0900

    쿼드트리, 이미 압축된 이미지를 입력하면 압축해제하여 출력
    단순 무식하게 재귀 호출로 문제를 해결했음. 하지만 이미지 사이즈가 말도안되게 커지면  시간도 많이 걸릴뿐더러 용량도 많이 차지하기 때문에 다른 방법을 찾아봐야함.

---
+++ QuadTree.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class QuadTree {
+	static char[][] decompressed = new char[16][16];
+	static String compressed;
+    
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		compressed = scanner.nextLine();
+		decompressed(0,0,16);
+		for(int i=0; i<16; i++) {
+			for(int j=0; j<16; j++) {
+				System.out.print(decompressed[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void decompressed(int x, int y,int size) {
+		char head = compressed.charAt(0);
+		
+		if(head == 'b' || head == 'w') {
+			for(int i=0; i<size; i++) {
+				for(int j=0; j<size; j++) {
+					decompressed[y+i][x+j] = head; 
+				}
+			}
+		}
+		else { //만약 x인 경우 
+			int half = size/2;
+			compressed = compressed.substring(1);
+			decompressed(x,y,half);
+			compressed = compressed.substring(1);
+			decompressed(x+half,y,half);
+			compressed = compressed.substring(1);
+			decompressed(x,y+half,half);
+			compressed = compressed.substring(1);
+			decompressed(x+half,y+half,half);
+		}
+	}
+	
+	private static void compressed() {
+		
+	}
+	
+	private static void reverse() {
+		for(int i=0; i<20; i++) {
+			for(int j=0; j<20; j++) {
+				
+			}
+		}
+	}
+
+}
