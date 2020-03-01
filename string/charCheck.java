fa0de4dc12be059f6ccb0e28aa748595577a5e0d
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-03-01 23:57:28 +0900

    기본 코드

---
+++ string/charCheck.java
@@ -0,0 +1,21 @@
+package dfsbfs;
+
+public class charCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "ATL";
+		String b = "SFO";
+		
+		if(a.compareTo(b)==1) System.out.println("a");
+		else System.out.println("b");
+				
+	   System.out.println(a.compareTo(b));
+	   System.out.println(b.compareTo(a));
+		
+		
+
+	}
+
+}
