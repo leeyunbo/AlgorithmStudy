a53cc5c00349c16d0a8ebe9aaf2802cbb608c23f
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-03-29 22:05:44 +0900

    순회를 이용한 문제 풀이

--- tree/GetPreOrder.java
+++ tree/GetPreOrder.java
@@ -20,37 +20,33 @@ public class GetPreOrder {
 		for(int j=0; j<line.length; j++) 
 			postOrder[j] = Integer.parseInt(line[j]);
 		
-		printPreOrder(inOrder, postOrder);
+		printPreOrder(0	, postOrder.length-1,0,postOrder.length-1);
 		br.close();
 	}
 	
-	private static void printPreOrder(int[] inOrder,int[] postOrder) {
-		int N = postOrder.length;
-		
-		if(postOrder.length == 0 && inOrder.length == 0) { 
+	private static void printPreOrder(int inStIdx, int inEndIdx, int poStIdx, int poEnIdx) {
+		if(poStIdx > poEnIdx || inStIdx > inEndIdx) {
 			return;
 		}
 		
-		int root = postOrder[N-1];
-	
+		int root = postOrder[poEnIdx];
+		
 		//1. root의 inOrder에서 위치를 찾는다, 그 왼쪽 값들은 다 왼쪽에 위치한 노드 들이며 오른쪽 값들은 다 오른쪽에 위치한 노드들이다. 
-		int rootIndex = 0;
-		for(int i=0; i<N; i++) {
+		int rootIdx = 0;
+		for(int i=inStIdx; i<=inEndIdx; i++) {
 			if(root == inOrder[i]) {
-				rootIndex = i;
+				rootIdx = i;
 				break;
 			}
 		}
 		
-		int leftLastIndex = rootIndex;
+		int left = rootIdx - inStIdx - 1;
+		int right = inEndIdx - rootIdx - 1;
 		
-		System.out.print(root + " "); //1. 부모를 출력한다.
-		printPreOrder(slice(inOrder,0,leftLastIndex),slice(postOrder,0,leftLastIndex)); //2. 왼쪽 자식을 출력한다.
-		printPreOrder(slice(inOrder,leftLastIndex+1,N), slice(postOrder,leftLastIndex,N-1)); //3. 오른쪽 자식을 출력한다. 
+		System.out.print(root + " "); //1. 부모를 출력한다
+		printPreOrder(inStIdx, inStIdx + left, poStIdx, poStIdx + left); //2. 왼쪽 자식을 출력한다.
+		printPreOrder(inEndIdx - right, inEndIdx, poEnIdx - right - 1, poEnIdx - 1 ); //3. 오른쪽 자식을 출력한다. 
 	}
 	
-	private static int[] slice(int[] array, int start, int end) {
-		return Arrays.copyOfRange(array, start, end);
-	}
 
 }
