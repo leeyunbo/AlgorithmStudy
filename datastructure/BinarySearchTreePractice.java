b4c927726fa588310022aca9570e705b61c1cccc
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-06-30 23:21:24 +0900

    [학습] 이진탐색트리 delete 수정

--- datastructure/BinarySearchTreePractice.java
+++ datastructure/BinarySearchTreePractice.java
@@ -16,23 +16,41 @@ public class BinarySearchTreePractice {
 	static Node root = null;
 	public static void main(String[] args) {
 		root = new Node(3);
-		insert(5, root); 
+		insert(5, root);
+		insert(4, root);
+		insert(6, root);
+		delete(4); 
+		printNode(root);
 		
+	}
+	
+	private static void printNode(Node current) {
+		if(current == null) return;
+		System.out.print(current.data + " ");
+		printNode(current.left);
+		printNode(current.right);
 	}
 	
 	
 	private static void insert(int data, Node parent) {
-		if(parent == null) parent = new Node(data); 
-		if(parent.data < data) {
-			insert(data, parent.right);
+		Node current = null;
+		while(parent != null) {
+			if(parent.data > data) {
+				current = parent;
+				parent = parent.left; 
+			}
+			else if(parent.data < data) {
+				current = parent;
+				parent = parent.right; 
+			}
+			else {
+				System.out.println("데이터 중복"); 
+				return; 
+			}
 		}
-		else if(parent.data > data) {
-			insert(data, parent.left); 
-		}
-		else {
-			System.out.println("데이터 중복");
-			return; 
-		}
+		
+		if(current.data < data) current.right = new Node(data);
+		else current.left = new Node(data);
 	}
 	
 	private static void contain(int data) {
@@ -77,15 +95,55 @@ public class BinarySearchTreePractice {
 	private static void delete(int data) {
 		Node deleteNode = search(data); 
 		Node parent = null;
+		boolean isLeft = false; 
+		
 		if(deleteNode == null) {
 			System.out.println("존재하지않습니다.");
 			return;
 		}
 		
-		// deleteNode의 부모를 찾아라! 
-		
-		if(deleteNode.left == null && deleteNode.right == null) {
-			
+		// deleteNode의 부모를 찾아라!
+		Node current = root; 
+		while(current != null) {
+			if(deleteNode.data < parent.data) {
+				current = current.left; 
+			}
+			else if(deleteNode.data > parent.data) {
+				current = current.right;
+			}
+			else break; 
 		}
+
+		if(parent.data > deleteNode.data) isLeft = true; 
+		
+		
+		// 삭제할 노드의 자식이 없으면 그냥 삭제하면 됨 
+		if(deleteNode.left == null && deleteNode.right == null) {
+			if(isLeft) parent.left = null; 
+			else parent.right = null; 
+		}
+		else {
+			// 왼쪽에 자식이 없거나, 오른쪽 자식이 없거나, 둘 다 있거나!
+			Node before = null;  
+			if(deleteNode.right == null) {
+				before = deleteNode.left; 
+				while(before.right != null) {
+					before = before.right; 
+				}
+			}
+			else {
+				before = deleteNode.right; 
+				while(before.left != null) {
+					before = before.left; 
+				}
+			}
+			
+			before.right = deleteNode.right;
+			before.left = deleteNode.left; 
+			if(isLeft) parent.left = before; 
+			else parent.right = before; 
+		}
+		
+		deleteNode = null;
 	}
 }
