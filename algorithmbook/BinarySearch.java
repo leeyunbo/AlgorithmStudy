d65aff217368910e4a8c18a8e6c0b5d906676ebd
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-07-09 13:20:32 +0900

    [알고리즘문제해결전략] 이진 탐색 정당성 증명(귀납법)

--- algorithmbook/BinarySearch.java
+++ algorithmbook/BinarySearch.java
@@ -1,29 +1,33 @@
 package algorithmbook;
 
+/*
+ * lo < hi가 무조건 성립해야함 
+ * A[lo] <= x <= A[hi] 가 성립해야함
+ */
+
 public class BinarySearch {
-	static int A[] = {1,2,3,4,5,6,7,8,9,10}; 
+	static int A[] = {1,2,3,4,5}; 
 	public static void main(String[] args) {
 		
-
+		System.out.println(binsearch(1));
 	}
 	
 	private static int binsearch(int data) {
 		int h = A.length-1;
 		int l = 0;
 		int m = 0;
-		while(true) {
-			m = h + l / 2;
+		
+		while(l < h) {
+			m = (h + l) / 2;
 			if(A[m] == data) break;
 			if(A[m] < data) {
-				
+				l = m + 1;
 			}
 			else {
-				
+				h = m; 
 			}
 		}
-		
-		
 		return m;
 	}
-
+	
 }
