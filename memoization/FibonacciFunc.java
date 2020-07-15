a7c73d10feb43192c41a864aa8029267f5918c56
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2020-07-16 00:16:52 +0900

    [백준 알고리즘] dp를 활용한 피보나치 수열, 0과 1 갯수 출력하기

--- memoization/FibonacciFunc.java
+++ memoization/FibonacciFunc.java
@@ -1,5 +1,10 @@
 package dp;
 
+/*
+ * https://www.acmicpc.net/problem/1003
+ * 백준 1003번 피보나치 함수 
+ */
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -21,36 +26,43 @@ public class FibonacciFunc {
 		for(int i=0; i<T; i++) {
 			zeroCnt = oneCnt = 0;
 			int n = Integer.parseInt(br.readLine());
-			cache = new long[n][1];
+			cache = new long[n+1][2];
 			fibonacci(n);
-			bw.write(cache[n][1] + " " + cache[n][2] + "\n");
+			bw.write(cache[n][0] + " " + cache[n][1] + "\n");
 		}
 		
 		bw.flush();
 	}
 	
 	
-	private static long fibonacci(int n) {
-		if(cache[n][0] > 0 || cache[n][1] > 0) {
-			zeroCnt += cache[n][0]; 
-			oneCnt += cache[n][1]; 
-			return 0;
-		}
-		
+	
+	// fib(2) = fib(1)에서 나온 0 혹은 1의 갯수 + fib(0)에서 나온 0 혹은 1의 갯수 
+	// fib(3) = fib(2)에서 나온 0 혹은 1의 갯수 + fib(1)에서 나온 0 혹은 1의 갯수 
+	private static void fibonacci(int n) {
 		if(n == 0) {
 			zeroCnt++;
-			return 0; 
+			cache[n][0] = 1;
+			return;
 		}
 		
 		if(n == 1) {
-			oneCnt++;
-			return 1; 
+			oneCnt++; 
+			cache[n][1] = 1;
+			return; 
 		}
 		
-		cache[n][0] = zeroCnt;
-		cache[n][1] = oneCnt; 
-		
-		return fibonacci(n-1) + fibonacci(n-2);
+		if(cache[n][0] > 0 || cache[n][1] > 0) {
+			zeroCnt += cache[n][0]; 
+			oneCnt += cache[n][1]; 
+			return; 
+		}
+		else {
+			fibonacci(n-1);
+			fibonacci(n-2);
+			cache[n][0] = zeroCnt; 
+			cache[n][1] = oneCnt; 
+		}
+	
 	}
 
 }
